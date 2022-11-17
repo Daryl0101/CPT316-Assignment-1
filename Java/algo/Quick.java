b49e54ba4f523ed39ec5829c0a93cfd08c338aa0
Daryl0101 <daryl.tneoh@gmail.com>
2022-11-18 02:24:06 +0800

    Sixth commit: Comments added, cleanup

--- Java/algo/Quick.java
+++ Java/algo/Quick.java
@@ -2,22 +2,17 @@ package Java.algo;
 import static Java.utility.utility.swap;
 
 public class Quick {
-    // Quick Sort Algorithm
-    public static void QuickSort(String[] word, int left, int right){
-        int pivot;
-
-        if(left<right){
-            pivot = Partition(word,left,right);
-            QuickSort(word,left,pivot-1);
-            QuickSort(word,pivot+1,right);
-        }
-    }
-
     // Partition Algorithm
     private static int Partition(String[] word, int left, int right){
+        // Set the rightmost word as the pivot word
         String pivot_word = word[right];
+        
+        // Initialize the index to place the words smaller than pivot word
         int i = left-1;
 
+        // Continuously compare if the word currently pointed by j is smaller or equal to the pivot word is found
+	    // Place the word to position i if it is smaller than the pivot word
+	    // Thus all words smaller than the pivot word is placed on the left of pivot word
         for(int j=left;j<right;j++){
             if(word[j].compareTo(pivot_word)<0){
                 i++;
@@ -25,7 +20,24 @@ public class Quick {
             }
         }
 
+        // Place the pivot word to the right of the last word smaller than the pivot word
+	    // Thus all words greater than the pivot word is placed on the right of the pivot word
         swap(word,i+1,right);
+
+        // Return the index of the new pivot
         return i+1;
+    }
+
+    // Quick Sort Algorithm
+    public static void QuickSort(String[] word, int left, int right){
+        // If the left index is smaller than the right index
+		// Choose a new pivot using Partition Algorithm
+		// Partition the list to the left and the right of the pivot word using Partition Algorithm
+        // Recursively sort the left partition and the right partition using Quick Sort
+        if(left<right){
+            int pivot = Partition(word,left,right);
+            QuickSort(word,left,pivot-1);
+            QuickSort(word,pivot+1,right);
+        }
     }
 }
--- Python/Quick.py
+++ Python/Quick.py
@@ -1,27 +1,33 @@
-import time
-
 # Partition Algorithm
 def Partition(word, left, right):
+	# Set the rightmost word as the pivot word
 	pivot_word = word[right]
 
-	i = left - 1
+	# Initialize the index to place the words smaller than pivot word
+	i = left-1
 
-	for j in range(left, right):
-		if word[j] <= pivot_word:
-			i = i + 1
+	# Continuously compare if the word currently pointed by j is smaller or equal to the pivot word is found
+	# Place the word to position i if it is smaller than the pivot word
+	# Thus all words smaller than the pivot word is placed on the left of pivot word
+	for j in range(left,right):
+		if word[j]<=pivot_word:
+			i = i+1
+			(word[i],word[j]) = (word[j],word[i])
 
-			(word[i], word[j]) = (word[j], word[i])
+	# Place the pivot word to the right of the last word smaller than the pivot word
+	# Thus all words greater than the pivot word is placed on the right of the pivot word
+	(word[i+1],word[right]) = (word[right],word[i+1])
 
-	(word[i + 1], word[right]) = (word[right], word[i + 1])
-
-	return i + 1
+	# Return the index of the new pivot
+	return i+1
 
 # Quick Sort Algorithm
 def QuickSort(word, left, right):
-		
-		if left < right:
-			pivot = Partition(word, left, right)
-
-			QuickSort(word, left, pivot - 1)
-
-			QuickSort(word, pivot + 1, right)
+		# If the left index is smaller than the right index
+		# Choose a new pivot using Partition Algorithm
+		# Partition the list to the left and the right of the pivot word using Partition Algorithm
+		# Recursively sort the left partition and the right partition using Quick Sort
+		if left<right:
+			pivot = Partition(word,left,right)
+			QuickSort(word,left,pivot-1)
+			QuickSort(word,pivot+1,right)
